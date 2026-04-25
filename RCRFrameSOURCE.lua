0cec89516bab3b66c2045e5fd2c207d680b62052
4leaker <levzubov549@gmail.com>
2026-04-25 11:47:56 +0300

    script

---
+++ RCRFrameSOURCE.lua
@@ -0,0 +1,17 @@
+getgenv().Config = {
+    victim = 91461245,             -- victim user id (like what skin u need) - example corax4 (coraxisgod)
+    helper = "",                   -- helper username (keep this empty if you want to spoof yourself)
+    level = 790,                    -- level override
+    streak = 150000,                   -- winstreak override (0 = disabled)
+    elo = 500000,                       -- elo override (0 = disabled)
+    keys = 10000000000000,                     -- keys override (0 = disabled)
+    premium = true,               -- spoof premium
+    verified = true,              -- spoof verified badge
+    unlockall = false,              -- unlock all (use with caution and doesnt work with shit executors)
+    platform = "DESKTOP",               -- DESKTOP / MOBILE / CONSOLE / VR
+    -- can be your discord invite ok ? also 
+    -- LEAVE CREDIT MADED BY ME (4LEAK)
+    -- script os you can skid it and just make stealer for xeno 😂
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/4leak/RCRNoWatermarkFrameScript/refs/heads/main/RCRFrameSOURCE.lua"))()
